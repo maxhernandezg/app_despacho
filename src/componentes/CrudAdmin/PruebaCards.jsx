53075c431f82c08c6e0ca70a2191846414a77a69
Maximiliano Hernández <max.hernandezg@duocuc.cl>
2026-06-25 19:33:21 -0400

    Prueba push antes de la prueba

--- src/componentes/CrudAdmin/PruebaCards.jsx
+++ src/componentes/CrudAdmin/PruebaCards.jsx
@@ -11,7 +11,7 @@ export const PruebaCards = () => {
     <section>
       <div className="flex justify-center">
         <CardComponent
-          title="Consultar Ordenes de compra 💰 (CI/CD)"
+          title="Consultar Ordenes de compra 💰 (CI/CD) 2"
           description="Revisa las últimas oc realizadas para generar su despacho"
           buttonText="Consultar"
           onClick={() => {
@@ -20,7 +20,7 @@ export const PruebaCards = () => {
           }}
         />
         <CardComponent
-          title="Revisar Ordenes de despacho 🚚 (CI/CD)"
+          title="Revisar Ordenes de despacho 🚚 (CI/CD) 2"
           description="Consulta los despachos realizados, modifica los registros de intentos o cierra la orden"
           buttonText="Consultar"
           onClick={() => {
